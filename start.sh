80fcb0764fb08f7a770078dbe9911930fb4184da
paullete <paulettenereah3@gmail.com>
2022-05-16 08:32:46 +0300

    minor changes

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 export EMAIL_USER='paullete.adhiambo@student.moringaschool.com'
 export EMAIL_PASS='borauhai@com'
 export SQLALCHEMY_DATABASE_URI='sqlite:///site.db'
-export SECRET_KEY='f9bf78b9a18ce6d46a0cd2b0b86df9da'
+export SECRET_KEY='potestuff'
 
 python3 manager.py server
